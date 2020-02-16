f1c5b95ad9f12b90e5135bfe658f24530317ff0d
Aun Johnsen <skippern@gimnechiske.org>
2020-02-16 17:02:32 +0100

    blah

--- lib/PTTBot.py
+++ lib/PTTBot.py
@@ -28,8 +28,8 @@ class PTTBot:
         @self.event
         async def on_ready():
             logger.info(f"[core]: Logged inn to Discord as {self.user.name}#{self.user.discriminator}")
-#            for i in self.get_all_channels():
-#                print(f"{i.name}")
+            for i in self.get_all_channels():
+                print(f"{i.name}")
 #            await radio.connect()
 
         @self.command(pass_context=True)
