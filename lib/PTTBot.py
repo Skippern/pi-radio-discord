9999e743f49b922d475d9e2f176c6f2a25537660
Aun Johnsen <skippern@gimnechiske.org>
2020-02-17 20:41:46 +0100

    anna device

--- lib/PTTBot.py
+++ lib/PTTBot.py
@@ -23,7 +23,9 @@ from lib.radioSource import *
 #RADIO_DEVICE = '/dev/snd/pcmC0D1p'
 #RADIO_DEVICE = '/dev/snd/pcmC0D2p'
 #RADIO_DEVICE = '/dev/snd/pcmC1D0c'
-RADIO_DEVICE = '/dev/snd/pcmC1D0p'
+#RADIO_DEVICE = '/dev/snd/pcmC1D0p'
+RADIO_DEVICE = '/dev/snd/controlC0'
+#RADIO_DEVICE = '/dev/snd/controlC1'
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
